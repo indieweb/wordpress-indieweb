22adf8e54537913aa095b821e234fc12455f663a
pfefferle <pfefferle@gmail.com>
2013-04-30 13:23:03 +0200

    added "webactions"

---
+++ indieweb-comments.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ Plugin Name: IndieWeb Comments
+ Plugin URI: https://github.com/pfefferle/wordpress-indieweb-comments
+ Description: Adds some IndieWeb functionality to WordPress' comment system
+ Author: pfefferle
+ Author URI: http://notizblog.org/
+ Version: 1.0.0-dev
+*/
+
+/**
+ * reply links with webactions support
+ *
+ * @link http://indiewebcamp.com/webactions
+ */
+function indieweb_reply_link( $link, $args, $comment, $post ) {
+  $permalink = get_permalink($post->ID);
+  
+  return "<action do='post reply' with='$permalink'>$link</action>";
+}
+add_filter('comment_reply_link', 'indieweb_reply_link', null, 4);
