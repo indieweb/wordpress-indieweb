2e6569d79ba44102c0adb5bcee41a56e9ed1b361
David Shanske <david@shanske.com>
2015-01-27 22:23:44 -0500

    Update to Automatically Generate POT using Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,26 @@ module.exports = function(grunt) {
       target: {
         files: {
           'readme.md': 'readme.txt'
-        },
-      },
-    }
-  });
+        }
+      }
+  },
+   makepot: {
+        target: {
+            options: {
+                mainFile: 'indieweb.php', // Main project file.
+                domainPath: '/languages',                   // Where to save the POT file.
+                potFilename: 'wordpress-indieweb.pot',
+                type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
+                updateTimestamp: true             // Whether the POT-Creation-Date should be updated without other changes.
+                }
+            }
+      }
+ });
 
   grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+  grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
 
   // Default task(s).
-  grunt.registerTask('default', ['wp_readme_to_markdown']);
+  grunt.registerTask('default', ['wp_readme_to_markdown', 'makepot']);
 };
