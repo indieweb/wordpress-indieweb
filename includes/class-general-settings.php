2c8283d9c25b218beb75b3347b04ae7c9280d8dc
David Shanske <david@shanske.com>
2016-02-06 10:13:12 -0600

    Rename General to Identity

--- includes/class-general-settings.php
+++ includes/class-general-settings.php
@@ -10,8 +10,8 @@ class IndieWeb_General_Settings {
 		 // Add General Options Page.
      add_submenu_page( 
         'indieweb',
-        __( 'General Options', 'indieweb' ), // page title
-        __( 'General Options', 'indieweb' ), // menu title
+        __( 'Options', 'indieweb' ), // page title
+        __( 'Options', 'indieweb' ), // menu title
         'manage_options', // access capability
                         $page,
                         array( 'IndieWeb_General_Settings', 'general_options_page' )
@@ -21,14 +21,13 @@ class IndieWeb_General_Settings {
 	public static function admin_settings() {
 		$page = 'iw_general_options';
 		// Settings Section
-		$section = 'iw_general_settings';
+		$section = 'iw_identity_settings';
     add_settings_section(
         $section,         // ID used to identify this section and with which to register options
-        'General Settings',                  // Title to be displayed on the administration page
-        array( 'IndieWeb_General_Settings', 'general_options_callback'), // Callback used to render the description of the section
+        'Identity Settings',                  // Title to be displayed on the administration page
+        array( 'IndieWeb_General_Settings', 'identity_options_callback'), // Callback used to render the description of the section
         $page                           // Page on which to add this section of options
     );
-
 
     register_setting( $section, 'iw_single_author');
     add_settings_field(
@@ -39,7 +38,7 @@ class IndieWeb_General_Settings {
       $section,         // The name of the section to which this field belongs
       array(                              // The array of arguments to pass to the callback. In this case, just a description.
         'name' => 'iw_single_author',
-				'description' => 'If this website represents a single individual, select this setting.'
+				'description' => 'If this website represents a single individual or entity, check this.'
       )
      );
 
@@ -53,25 +52,38 @@ class IndieWeb_General_Settings {
     	$section         // The name of the section to which this field belongs
 		);
 
+    register_setting( $section, 'iw_relmehead');
+    add_settings_field(
+      'iw_relmehead',                      // ID used to identify the field throughout the theme
+      'Hidden Rel-Me Links',                           // The label to the left of the option interface element
+      array( 'IndieWeb_General_Settings', 'checkbox_callback' ),   // The name of the function responsible for rendering the option interface
+      $page,                          // The page on which this option will be displayed
+      $section,         // The name of the section to which this field belongs
+      array(                              // The array of arguments to pass to the callback. In this case, just a description.
+        'name' => 'iw_relmehead',
+        'description' => 'This will embed rel-me links into the page invisibly. Disables other options such as the Rel-Me Widget.'
+      )
+     );
 
 
 
 	}
 
 
-  public static function general_options_callback() {
+  public static function identity_options_callback() {
     echo '<p>';
-    esc_html_e( 'Provides support for rel-me links, either through a widget or invisibly adding them to the page.', 'indieweb' );
+    esc_html_e( 'Using rel=me on a link indicates the link represents the same person or entity as the current page. Depending on whether this is a single author or multi-author site, these links will either be displayed on the main page or on the specific page representing the author on the site.', 'indieweb' );
     echo '</p>';
   }
 
   public static function general_options_page() {
+		// If this is not a multi-author site, always set single_author to checked.
+		if ( ! is_multi_author() ) {
+			set_option('iw_single_author', 1);
+		}
     echo '<div class="wrap">';
-    echo '<h2>' . esc_html__( 'General Options', 'indieweb' ) . '</h2>';
-    echo '<p>';
-    echo '</p><hr />';
     echo '<form method="post" action="options.php">';
-    settings_fields( 'iw_general_settings' );
+    settings_fields( 'iw_identity_settings' );
     do_settings_sections( 'iw_general_options' );
     submit_button();
     echo '</form></div>';
