516ffc379fc3f9d443ba107856612c80b5258134
Matthias Pfefferle <pfefferle@gmail.com>
2016-09-23 22:13:40 +0200

    indents!!!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,62 @@
 module.exports = function (grunt) {
-	// Project configuration.
-	grunt.initConfig({
-		wp_readme_to_markdown: {
-			target: {
-				files: {
-					'readme.md': 'readme.txt'
-				}
-			}
-		},
+  // Project configuration.
+  grunt.initConfig({
+    wp_readme_to_markdown: {
+      target: {
+        files: {
+          'readme.md': 'readme.txt'
+        }
+      }
+    },
 
-		copy: {   
-           main: {
-               options: {
-                   mode: true
-               },
-               src: [
-                   '**',
-                   '!node_modules/**',
-                   '!build/**',
-                   '!.git/**',
-                   '!Gruntfile.js',
-                   '!package.json',
-                   '!.gitignore'
-               ],
-               dest: 'build/trunk/'
-           }
-       },
+    copy: {
+      main: {
+        options: {
+          mode: true
+        },
+        src: [
+          '**',
+          '!node_modules/**',
+          '!build/**',
+          '!.git/**',
+          '!Gruntfile.js',
+          '!package.json',
+          '!.gitignore'
+        ],
+        dest: 'build/trunk/'
+      }
+    },
 
     sass: {                              // Task
-       dist: {                            // Target
-         options: {                       // Target options
-             style: 'compressed'
-             },
-          files: {                         // Dictionary of files
-        'css/indieweb.css': 'sass/main.scss',       // 'destination': 'source'
-				'css/indieweb-bw.css': 'sass/main-bw.scss'
-         }
-  }
-  },
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed'
+        },
+        files: {                         // Dictionary of files
+          'static/css/indieweb.css': 'sass/main.scss',       // 'destination': 'source'
+          'static/css/indieweb-bw.css': 'sass/main-bw.scss'
+        }
+      }
+    },
 
+    makepot: {
+      target: {
+        options: {
+          mainFile: 'indieweb.php',
+          domainPath: '/languages',
+          exclude: ['build/.*'],
+          potFilename: 'wordpress-indieweb.pot',
+          type: 'wp-plugin',
+          updateTimestamp: true
+        }
+      }
+    }
+  });
 
-		makepot: {
-			target: {
-				options: {
-					mainFile: 'indieweb.php',
-					domainPath: '/languages',
-					exclude: ['build/.*'],
-					potFilename: 'wordpress-indieweb.pot',
-					type: 'wp-plugin',
-					updateTimestamp: true
-				}
-			}
-		}
-	});
-
-	grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
-	grunt.loadNpmTasks('grunt-wp-i18n');
+  grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+  grunt.loadNpmTasks('grunt-wp-i18n');
   grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-sass');
-	// Default task(s).
-	grunt.registerTask('default', ['wp_readme_to_markdown', 'makepot']);
+  grunt.loadNpmTasks('grunt-sass');
+  // Default task(s).
+  grunt.registerTask('default', ['wp_readme_to_markdown', 'makepot']);
 };
